a9f14c16bdc24a5d0906ab04f6152528fd41a2a9
🖐 <147812434+dnoe-pro@users.noreply.github.com>
2026-09-25 15:37:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# minishop-php
+
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$action = isset($_GET['a']) ? strtolower($_GET['a']) : 'home';
+
+$fiche_ctrl = 'src/controllers/c.' . $action . '.php';
+$fiche_view = 'src/views/v.' . $action . '.php';
+
+if (!file_exists($fiche_ctrl) || !file_exists($fiche_view)) {
+    header('Location: ?a=erreurs&code=404');
+    exit;
+}
+
+require $fiche_ctrl;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="public/css/index.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php require $fiche_view; ?>
+</body>
+
+</html>
